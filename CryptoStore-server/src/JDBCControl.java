48c061e0afde97a58d6b59ee69cf0f5f0c3e6270
HarryS <Harry@harrys-mbp.home>
2015-10-08 01:34:17 +0100

    clean up

--- CryptoStore-server/src/JDBCControl.java
+++ CryptoStore-server/src/JDBCControl.java
@@ -2,38 +2,26 @@ import java.sql.*;
 
 public class JDBCControl {
     public JDBCControl(){
-        try (
-                // Step 1: Allocate a database "Connection" object
-                Connection conn = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:8888/users", "admin", "locH4al"); // MySQL
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:8888/users", "admin", "locH4al");
 
-                // Step 2: Allocate a "Statement" object in the Connection
-                Statement stmt = conn.createStatement();
-        ) {
-            // Step 3: Execute a SQL SELECT query, the query result
-            //  is returned in a "ResultSet" object.
+            Statement stmt = conn.createStatement();
+
             String strSelect = "select id, username, password from users";
-            System.out.println("The SQL query is: " + strSelect); // Echo For debugging
-            System.out.println();
-
             ResultSet rset = stmt.executeQuery(strSelect);
 
-            // Step 4: Process the ResultSet by scrolling the cursor forward via next().
-            //  For each row, retrieve the contents of the cells with getXxx(columnName).
-            System.out.println("The records selected are:");
             int rowCount = 0;
-            while(rset.next()) {   // Move the cursor to the next row
+            while(rset.next()) {
                 int id = rset.getInt("id");
                 String username = rset.getString("username");
                 String password = rset.getString("password");
-                System.out.println(id + ", " + username + ", " + password);
+                System.out.println(id + " : " + username + " : " + password);
                 ++rowCount;
             }
-            System.out.println("Total number of records = " + rowCount);
+            System.out.println("\nTotal number of records: " + rowCount);
 
         } catch(SQLException ex) {
             ex.printStackTrace();
         }
-        // Step 5: Close the resources - Done automatically by try-with-resources
     }
 }
