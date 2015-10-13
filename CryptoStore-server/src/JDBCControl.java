0d65229867d70e97438fbde0d750d255997159f8
HarryS <Harry@harrys-mbp.home>
2015-10-13 22:20:09 +0100

    JDBC control updated to add, check if exists and password matching the users

--- CryptoStore-server/src/JDBCControl.java
+++ CryptoStore-server/src/JDBCControl.java
@@ -1,4 +1,6 @@
 import java.sql.*;
+import java.util.Arrays;
+import java.util.Base64;
 
 public class JDBCControl {
     private static String DBHost;
@@ -11,13 +13,36 @@ public class JDBCControl {
         JDBCControl.serverPassword =  serverPassword;
     }
 
+    public static boolean usernameExists(String username) {
+        boolean exists = false;
+
+        try {
+            Connection connection = DriverManager.getConnection(DBHost, serverUsername, serverPassword);
+
+            String query =  "SELECT username " +
+                            "FROM user_credentials " +
+                            "WHERE username = ?";
+
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, username);
+
+            ResultSet queryResult = statement.executeQuery();
+
+            exists = queryResult.next();
+        } catch (SQLException ex) {
+            Error.NO_USER.print();
+        }
+
+        return exists;
+    }
+
     public static boolean checkUserPassword(String user, String password) {
         boolean verify = false;
 
         try {
             Connection connection = DriverManager.getConnection(DBHost, serverUsername, serverPassword);
 
-            String query =  "SELECT id, username, hash " +
+            String query =  "SELECT username, hash " +
                             "FROM user_credentials";
 
             Statement statement = connection.prepareStatement(query);
@@ -36,19 +61,54 @@ public class JDBCControl {
         return verify;
     }
 
-    public static boolean createNewUser(String username, String passwdHash) {
+    public static boolean createNewUser(String username, String passwdHash, byte[] salt) {
         try {
             Connection connection = DriverManager.getConnection(DBHost, serverUsername, serverPassword);
 
-            String query =  "INSERT INTO user_credentials (username, hash) " +
-                            "VALUES (\'" + username + "\', \'" + passwdHash + "\')";
+            String query =  "INSERT INTO user_credentials (username, hash, salt) " +
+                            "VALUES (?, ?, ?)";
 
-            connection.prepareStatement(query).execute();
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, username);
+            statement.setString(2, passwdHash);
+            statement.setString(3, Base64.getEncoder().encodeToString(salt));
+System.out.println(statement.toString());
+            statement.execute();
 
         } catch (SQLException ex) {
             Error.NO_USER.print();
         }
 
         return checkUserPassword(username, passwdHash);
+    }
+
+    public static byte[] getSalt(String username) {
+        String salt = null;
+
+        try {
+            Connection connection = DriverManager.getConnection(DBHost, serverUsername, serverPassword);
+
+            String query =  "SELECT salt " +
+                            "FROM user_credentials " +
+                            "WHERE username = ? ";
+
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, username);
+
+            ResultSet queryResult = statement.executeQuery();
+
+            while (queryResult.next()) {
+                salt = queryResult.getString("salt");
+            }
+
+        } catch (SQLException ex) {
+            Error.NO_USER.print();
+        }
+
+        if (salt.length() > 0) {
+            return Base64.getDecoder().decode(salt);
+        } else {
+            return null;
+        }
     }
 }
--- CryptoStore-server/src/Server.java
+++ CryptoStore-server/src/Server.java
@@ -1,13 +1,16 @@
 class Server {
-
     public static void main(String args[]) throws Exception {
-        new ServerManager(5556);
+//        new ServerManager(5556);
 
-//        System.out.println(HashGenerator.getHash("fD462^gd", HashGenerator.getSalt(), 100000, 64));
+        /**HashGenerator testing**/
+        byte[] salt = HashGenerator.getSalt();
+        String hashedpass = HashGenerator.getHash("P4$$w0rd", salt, 100000, 32);
 
-//        JDBCControl jdbcControl = new JDBCControl("jdbc:mysql://mysql.student.sussex.ac.uk:3306/cs391", "cs391", "r127xxhar1");
-//        System.out.println(jdbcControl.checkUserPassword("HarryAdmin", "passwordHash01passwordHash01passwordHash01passwordHash0111111111"));
-//        System.out.println(jdbcControl.createNewUser("NonAdmin", "passwordHash01passwordHash01passwordHash01passwordHash0122222222"));
-//        System.out.println(new HashGenerator().getHashString(new HashGenerator().getHash("paSSw@", "sexySalt".getBytes(), 1000, 32)));
+        /**JDBCControl testing**/
+        new JDBCControl("jdbc:mysql://mysql.student.sussex.ac.uk:3306/cs391", "cs391", "r127xxhar1");
+
+        System.out.println(JDBCControl.createNewUser("Admin2", hashedpass, salt));
+        System.out.println(JDBCControl.checkUserPassword("Admin2", HashGenerator.getHash("P4$$w0rd", JDBCControl.getSalt("Admin2"), 100000, 32)));
+        System.out.println(JDBCControl.usernameExists("Admin4"));
     }
 } 
