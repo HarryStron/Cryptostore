334bae38bfc614488906ff6a8d915f04273f21ae
Harrys <HarryS@GitBucket.com>
2015-10-26 17:39:02 +0000

    keep classes clean of testing code

--- CryptoStore-client/src/Client.java
+++ CryptoStore-client/src/Client.java
@@ -9,18 +9,18 @@ public class Client {
 //        clientManager.sendFile("test.txt");
 //        clientManager.retrieveFile("test.txt");
 
-        try {
-            byte[] filebytes = Encryption.encryptFile("password".toCharArray(), Paths.get("test.txt"));
-            FileOutputStream fos = new FileOutputStream("testEncrypted.txt");
-            fos.write(filebytes);
-            fos.close();
-
-            byte[] filebytes2 = Encryption.decryptFile("password".toCharArray(), Paths.get("testEncrypted.txt"));
-            FileOutputStream fos2 = new FileOutputStream("testDecrypted.txt");
-            fos2.write(filebytes2);
-            fos2.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        try {
+//            byte[] filebytes = Encryption.encryptFile("password".toCharArray(), Paths.get("test.txt"));
+//            FileOutputStream fos = new FileOutputStream("testEncrypted.txt");
+//            fos.write(filebytes);
+//            fos.close();
+//
+//            byte[] filebytes2 = Encryption.decryptFile("password".toCharArray(), Paths.get("testEncrypted.txt"));
+//            FileOutputStream fos2 = new FileOutputStream("testDecrypted.txt");
+//            fos2.write(filebytes2);
+//            fos2.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 }
